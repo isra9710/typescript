34f274b0c94ba828945574aa6b06db96c45e4687
isra9710 <isra.rios.con@gmail.com>
2021-11-03 12:47:25 -0600

    fifth commit

---
+++ 9/creando_namespace.ts
@@ -0,0 +1,21 @@
+console.log(
+    Validaciones.validarTexto("Barry Allen")
+);
+
+if(Validaciones.validarTexto("Barry Allen")){
+    console.log("El texto es valido");
+}else{
+   console.log("El texto no es valido"); 
+}
+
+let hoy = new Date();
+
+console.log(
+    Validaciones.validarFecha(hoy)
+);
+
+if(Validaciones.validarFecha(hoy)){
+    console.log("La fecha es valida");
+}else{
+    console.log("La fecha no es valida");
+}
---
+++ 9/validaciones/fechas.ts
@@ -0,0 +1,9 @@
+namespace Validaciones{
+
+    export function validarFecha(fecha:Date):boolean{
+        if(isNaN(fecha.valueOf())){
+            return false;
+        }
+        return true;
+    }
+}
