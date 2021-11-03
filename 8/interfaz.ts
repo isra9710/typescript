06f42b75723a07091c3af75ccb37bac1f96e5d0b
isra9710 <isra.rios.con@gmail.com>
2021-11-02 18:28:37 -0600

    second commit

---
+++ 8/interfaz.ts
@@ -0,0 +1,10 @@
+function enviarMision(xmen:any){
+    console.log("Enviando a " + xmen.nombre);
+}
+
+let wolverine = {
+    nombre: "Wolverine",
+    poder:"Regeneración"
+};
+
+enviarMision(wolverine);
