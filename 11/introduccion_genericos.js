23fa82a4211b9b831806984900613a42dba6ba01
isra9710 <isra.rios.con@gmail.com>
2021-11-04 12:02:55 -0600

    eighth commit

---
+++ 11/introduccion_genericos.js
@@ -0,0 +1,8 @@
+"use strict";
+function imprimirConsola(parametro) {
+    console.log(parametro);
+}
+imprimirConsola(123);
+imprimirConsola(new Date());
+imprimirConsola({});
+imprimirConsola({ nombre: "Fernando" });
---
+++ 11/introduccion_genericos.ts
@@ -0,0 +1,8 @@
+function imprimirConsola(parametro:any){
+    console.log(parametro);
+}
+
+imprimirConsola(123);
+imprimirConsola(new Date());
+imprimirConsola({});
+imprimirConsola({nombre:"Fernando"});
